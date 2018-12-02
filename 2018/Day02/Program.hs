4e8e6e67a92436ad544acd5ddb8ffa86c08ce62a
Johannes Egger <j.egger@posteo.at>
2018-12-02 21:01:24 +0100

    Improve day 2 after learning from @CarstenKoenig

--- 2018/Day02/Program.hs
+++ 2018/Day02/Program.hs
@@ -1,7 +1,6 @@
-import qualified Data.Map.Strict as Map
-import Data.Map.Strict (Map)
-import Data.List (tails, find)
+import Data.List (tails, find, group, sort)
 import Data.Maybe (mapMaybe, listToMaybe)
+import Data.Bool (bool)
 
 main :: IO ()
 main = do
@@ -15,16 +14,9 @@ readInput = lines <$> readFile "input.txt"
 solve1 :: [String] -> Int
 solve1 input = (occurrences 2) * (occurrences 3)
     where
-        occurrences count = sum $ map (boolToInt . elem count) allOccurrences
-        allOccurrences = map (getOccurrences Map.empty) input
-        getOccurrences m [] = map snd $ Map.toList m
-        getOccurrences m (c : xs) =
-            let m' = Map.alter insertOrUpdateOccurrence c m in
-            getOccurrences m' xs
-        insertOrUpdateOccurrence (Just v) = Just $ v + 1
-        insertOrUpdateOccurrence Nothing = Just 1
-        boolToInt False = 0
-        boolToInt True = 1
+        occurrences count = sum $ map (bool 0 1 . elem count) allOccurrences
+        allOccurrences = map getOccurrences input
+        getOccurrences = map length . group . sort
 
 solve2 :: [String] -> Maybe String
 solve2 input = listToMaybe $ mapMaybe getIntersectionWithOneLetterDiff allPairs
@@ -34,7 +26,7 @@ solve2 input = listToMaybe $ mapMaybe getIntersectionWithOneLetterDiff allPairs
             | length intersection == length a - 1 = Just intersection
             | otherwise = Nothing
             where
-                intersection = mapMaybe intersectCharacter $ zip a b
-                intersectCharacter (a, b)
-                    | a == b = Just a
-                    | otherwise = Nothing
+                intersection = zippedIntersection a b
+
+zippedIntersection :: Eq a => [a] -> [a] -> [a]
+zippedIntersection a b = [ a' | (a', b') <- zip a b, a' == b' ]
