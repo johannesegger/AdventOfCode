7898fc4d421a72d07ecb1dc50d8e69827535da74
Johannes Egger <j.egger@posteo.at>
2019-07-30 09:20:30 +0200

    Solve 2018/11 using summed-area table

--- 2018/Day11/Program.hs
+++ 2018/Day11/Program.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE TupleSections #-}
-
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict ((!?))
+import Data.Maybe (fromMaybe)
 import Data.List (maximumBy)
 import Data.Ord (comparing)
 
@@ -18,22 +19,31 @@ solve1 = fst $ maximumBy (comparing snd) $ calculateTotalPower <$> topLeftCells
         calculateTotalPower (x, y) =
             let totalPower = sum $ getPower <$> getSquare 3 (x, y) in
             ((x, y), totalPower)
+        getSquare size (x, y) = [ (x + dx, y + dy) | dx <- [0..size - 1], dy <- [0..size - 1] ]
 
 solve2 :: (((Int, Int), Int), Int)
-solve2 = maximumBy (comparing snd) $ calculateMaxTotalPowerFromCell <$> topLeftCells
+solve2 = maximumBy (comparing snd) $ calculateMaxTotalPowerFromCell <$> cells
     where
         totalSize = 300
-        topLeftCells = [ (x, y) | x <- [1..totalSize], y <- [1..totalSize] ]
+        cells = [ (x, y) | x <- [1..totalSize], y <- [1..totalSize] ]
+        powerMap = foldl updateSummedAreaTable Map.empty cells
+        updateSummedAreaTable m (x, y) =
+            let cellPower = getPower (x, y)
+                sum1 = fromMaybe 0 $ m !? (x, y - 1)
+                sum2 = fromMaybe 0 $ m !? (x - 1, y)
+                sum3 = fromMaybe 0 $ m !? (x - 1, y - 1)
+                power = cellPower + sum1 + sum2 - sum3
+            in
+                Map.insert (x, y) power m
         calculateMaxTotalPowerFromCell (x, y) =
-            let (size, power) = maximumBy (comparing snd) $ scanl (calculateTotalPower (x, y)) (0, 0) $ replicate (totalSize - max x y + 1) () in
+            let (size, power) = maximumBy (comparing snd) $ calculateTotalPower (x, y) <$> [1..totalSize - max x y + 1] in
             (((x, y), size), power)
-        calculateTotalPower (startX, startY) (size, power) () =
-            let horizontalPower = sum $ getPower . (, startY + size) <$> [startX..startX + size]
-                verticalPower = sum $ getPower . (startX + size, ) <$> [startY..startY + size - 1]
-            in (size + 1, power + horizontalPower + verticalPower)
-
-getSquare :: Int -> (Int, Int) -> [(Int, Int)]
-getSquare size (x, y) = [ (x + dx, y + dy) | dx <- [0..size - 1], dy <- [0..size - 1] ]
+        calculateTotalPower (startX, startY) size =
+            let leftTopPower = fromMaybe 0 $ powerMap !? (startX - 1, startY - 1)
+                rightTopPower = fromMaybe 0 $ powerMap !? (startX + size - 1, startY - 1)
+                leftBottomPower = fromMaybe 0 $ powerMap !? (startX - 1, startY + size - 1)
+                rightBottomPower = fromMaybe 0 $ powerMap !? (startX + size - 1, startY + size - 1)
+            in (size, rightBottomPower - leftBottomPower - rightTopPower + leftTopPower)
 
 getPower :: (Int, Int) -> Int
 getPower (x, y) =
