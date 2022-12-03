a5a9cf23e72adaca03186a19ac70e7c3d5639f5c
Johannes Egger <j.egger@posteo.at>
2022-12-03 22:27:14 +0100

    Day03 F#

---
+++ 2022/Day03/F#/Program.fs
@@ -0,0 +1,32 @@
+﻿open System.IO
+open System.Diagnostics
+
+let getPrio c =
+    Debug.Assert((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+
+    if c >= 'a' && c <= 'z' then (int c) - (int 'a') + 1
+    else (int c) - (int 'A') + 27
+
+let lines = File.ReadAllLines("input.txt")
+
+lines
+|> Seq.map (fun line ->
+    let compartmentA = line.Substring(0, line.Length / 2) |> Set.ofSeq
+    let compartmentB = line.Substring(line.Length / 2) |> Set.ofSeq
+    Set.intersect compartmentA compartmentB |> Seq.exactlyOne
+    |> getPrio
+)
+|> Seq.sum
+|> printfn "Part 1: %d"
+
+lines
+|> Seq.chunkBySize 3
+|> Seq.map (fun chunk ->
+    chunk
+    |> Seq.map Set.ofSeq
+    |> Seq.reduce Set.intersect
+    |> Seq.exactlyOne
+    |> getPrio
+)
+|> Seq.sum
+|> printfn "Part 2: %d"
