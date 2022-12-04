faa2ca3b21fc8e37c1866b2bc3842aa3a8f5f105
Johannes Egger <j.egger@posteo.at>
2022-12-04 21:17:43 +0100

    Day04 F#

--- 2022/Day04/F#/Program.fs
+++ 2022/Day04/F#/Program.fs
@@ -1,4 +1,25 @@
 ﻿open System.IO
 
-File.ReadLines("input.txt")
+let parse (line: string) =
+    let parts = line.Split(',') |> Array.map (fun v -> v.Split('-') |> Array.map int)
+    (parts.[0].[0], parts.[0].[1]),
+    (parts.[1].[0], parts.[1].[1])
+
+let input =
+    File.ReadLines("input.txt")
+    |> Seq.map parse
+    |> Seq.toList
+
+input
+|> Seq.filter (fun ((x1, x2), (y1, y2)) ->
+    (x1 <= y1 && x2 >= y2) || (y1 <= x1 && y2 >= x2)
+)
+|> Seq.length
 |> printfn "Part 1: %A"
+
+input
+|> Seq.filter (fun ((x1, x2), (y1, y2)) ->
+    (x1 <= y2 && x2 >= y1) || (y1 <= x2 && y2 >= x1)
+)
+|> Seq.length
+|> printfn "Part 2: %A"
