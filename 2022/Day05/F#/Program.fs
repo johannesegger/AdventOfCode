d07cca26b01057c8748f30befd77fd35a80495c9
Johannes Egger <eggj@htlvb.at>
2022-12-05 11:48:49 +0100

    Clean up Day05 F#

--- 2022/Day05/F#/Program.fs
+++ 2022/Day05/F#/Program.fs
@@ -2,24 +2,35 @@
 open System.IO
 open System.Text.RegularExpressions
 
+module String =
+    let rev v = v |> Seq.rev |> Seq.toArray |> String
+
+type Stack = Stack of string
+module Stack =
+    let pop count (Stack v) =
+        Stack (v.Substring(0, count)),
+        Stack (v.Substring(count))
+    let push (Stack source) (Stack target) =
+        String.rev source + target
+        |> Stack
+    let pushAll (Stack source) (Stack target) =
+        source + target
+        |> Stack
+    let peek (Stack v) = v.[0]
+
 let parseStacks (lines: string array) =
-    let stackWidth = 4
-    Seq.initInfinite (fun i -> i * stackWidth + 1)
-    |> Seq.takeWhile (fun i -> i < lines.[0].Length)
-    |> Seq.map (fun i ->
-        lines
-        |> Seq.map (fun line -> line.[i])
-        |> Seq.filter Char.IsLetter
-        |> Seq.toList
-    )
-    |> Seq.toList
+    lines
+    |> Seq.transpose
+    |> Seq.map (Seq.toArray >> String)
+    |> Seq.filter (fun v -> Char.IsDigit v.[v.Length - 1])
+    |> Seq.map (fun v -> v.Substring(0, v.Length - 1).TrimStart() |> Stack)
+    |> Seq.toArray
 
 let lines = File.ReadAllLines("input.txt")
 
 let stacks =
     lines
     |> Array.takeWhile (not << String.IsNullOrEmpty)
-    |> fun v -> Array.take (v.Length - 1) v
     |> parseStacks
 
 let parseMove line =
@@ -36,20 +47,16 @@ let moves =
     |> Array.map parseMove
     |> Array.toList
 
-let executeMove9000 (stacks: char list list) (count, source, target) =
-    let sourceStack = stacks.[source - 1]
-    let newSourceStack = sourceStack |> List.skip count
-    let newTargetStack =
-        let sourceCrates = sourceStack |> List.take count
-        List.rev sourceCrates @ stacks.[target - 1]
+let executeMove9000 (stacks: Stack array) (count, source, target) =
+    let (popped, newSourceStack) = Stack.pop count stacks.[source - 1]
+    let newTargetStack = Stack.push popped stacks.[target - 1]
     stacks
-    |> List.updateAt (source - 1) newSourceStack
-    |> List.updateAt (target - 1) newTargetStack
+    |> Array.updateAt (source - 1) newSourceStack
+    |> Array.updateAt (target - 1) newTargetStack
 
-let getTopCrates (stacks: char list list) =
+let getTopCrates stacks =
     stacks
-    |> List.map List.head
-    |> List.toArray
+    |> Array.map Stack.peek
     |> String
 
 (stacks, moves)
@@ -57,10 +64,12 @@ let getTopCrates (stacks: char list list) =
 |> getTopCrates
 |> printfn "Part 1: %s"
 
-let executeMove9001 (stacks: char list list) (count, source, target) =
+let executeMove9001 (stacks: Stack array) (count, source, target) =
+    let (popped, newSourceStack) = Stack.pop count stacks.[source - 1]
+    let newTargetStack = Stack.pushAll popped stacks.[target - 1]
     stacks
-    |> List.updateAt (target - 1) (List.take count stacks.[source - 1] @ stacks.[target - 1])
-    |> List.updateAt (source - 1) (List.skip count stacks.[source - 1])
+    |> Array.updateAt (source - 1) newSourceStack
+    |> Array.updateAt (target - 1) newTargetStack
 
 (stacks, moves)
 ||> List.fold executeMove9001
