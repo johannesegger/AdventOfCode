9355d65992065e922df12cd3453d2b8b89891556
Johannes Egger <j.egger@posteo.at>
2022-12-06 06:09:39 +0100

    Day06 F#

--- 2022/Day06/F#/Program.fs
+++ 2022/Day06/F#/Program.fs
@@ -1,4 +1,15 @@
 ﻿open System.IO
 
-File.ReadLines("input.txt")
-|> printfn "%A"
+let input = File.ReadAllText("input.txt")
+
+let findMarker numberOfDistinctCharacters =
+    input
+    |> Seq.windowed numberOfDistinctCharacters
+    |> Seq.findIndex (fun v -> v |> Set.ofSeq |> Set.count = numberOfDistinctCharacters)
+    |> fun i -> i + numberOfDistinctCharacters
+
+findMarker 4
+|> printfn "Part 1: %d"
+
+findMarker 14
+|> printfn "Part 2: %d"
