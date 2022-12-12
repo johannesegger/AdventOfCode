cc17b908052844b34f5960ac5876eb87eec124e9
Johannes Egger <eggj@htlvb.at>
2022-12-12 12:23:07 +0100

    Day12 F# (needs clean up)

--- 2022/Day12/F#/Program.fs
+++ 2022/Day12/F#/Program.fs
@@ -1,6 +1,17 @@
 ﻿open System.IO
 
-let map = File.ReadAllLines("sample.txt")
+let map = File.ReadAllLines("input.txt")
+
+let getPositions c =
+    map
+    |> Seq.indexed
+    |> Seq.choose (fun (y, line) ->
+        match Seq.tryFindIndex (fun v -> v = c) line with
+        | Some x -> Some (x, y)
+        | None -> None
+    )
+let startPosition = getPositions 'S' |> Seq.exactlyOne
+let endPosition = getPositions 'E' |> Seq.exactlyOne
 
 let getHeight c =
     if c = 'S' then 'a'
@@ -15,31 +26,45 @@ let tryCharAt (x, y) =
     |> Seq.tryItem y
     |> Option.bind (Seq.tryItem x)
 
-let getNextSteps (x, y) visitedPositions =
+let getNextSteps (x, y) =
     let currentHeight = charAt (x, y) |> getHeight
     [ (x - 1, y); (x + 1, y); (x, y - 1); (x, y + 1) ]
-    |> List.filter (fun v -> not <| Set.contains v visitedPositions)
     |> List.choose (fun v ->
         match tryCharAt v |> Option.map getHeight with
-        | Some c when (int c = int currentHeight || int c = int currentHeight + 1) -> Some v
+        | Some c when int c <= int currentHeight + 1 -> Some v
         | _ -> None
     )
 
-let rec makeMoves steps visitedPositions currentPosition =
-    let currentHeight = charAt currentPosition
-    if currentHeight = 'E' then
-        printfn "%d" steps
-        Some steps
-    else
-        let visitedPositions' = Set.add currentPosition visitedPositions
-        let next = getNextSteps currentPosition visitedPositions'
-        // printfn "%A -> %A" currentPosition next
-        next
-        |> List.map (makeMoves (steps + 1) visitedPositions')
-        |> List.choose id
-        |> function
-        | [] -> None
-        | x -> x |> List.min |> Some
+let combineKnownPositions a b =
+    a
+    |> Map.toSeq
+    |> Seq.fold (fun knownPositions (k, v) ->
+        knownPositions
+        |> Map.change k (function
+            | Some v' when v' < v -> Some v'
+            | _ -> Some v
+        )
+    ) b
 
-makeMoves 0 Set.empty (0, 0)
-|> printfn "Part 1: %A"
+let rec makeMoves steps knownPositions currentPosition =
+    match Map.tryFind currentPosition knownPositions with
+    | Some v when v <= steps -> knownPositions
+    | _ ->
+        if charAt currentPosition = 'E' then
+            combineKnownPositions (Map.ofList [(currentPosition, steps)]) knownPositions
+        else
+            let knownPositions' = Map.add currentPosition steps knownPositions
+            let next = getNextSteps currentPosition
+            next
+            |> List.fold (fun knownPositions next ->
+                makeMoves (steps + 1) knownPositions next
+            ) knownPositions'
+
+makeMoves 0 Map.empty startPosition
+|> Map.find endPosition
+|> printfn "Part 1: %d"
+
+getPositions 'a'
+|> Seq.fold (makeMoves 0) Map.empty
+|> Map.find endPosition
+|> printfn "Part 2: %d"
