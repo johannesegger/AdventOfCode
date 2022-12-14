2a85ef98add30a40837986131ba484236ecc0bc7
Johannes Egger <eggj@htlvb.at>
2022-12-14 09:03:14 +0100

    Day 14 F# Part 2

--- 2022/Day14/F#/Program.fs
+++ 2022/Day14/F#/Program.fs
@@ -30,24 +30,46 @@ let parseRockLines state line =
     getRockCoordinates line
     |> Seq.fold addRockCoordinates state
 
-let rec findRestPosition (x, y) map =
-    if Set.exists (fun (x', y') -> y' > y) map then
-        if Set.contains (x, y + 1) map then
-            if Set.contains (x - 1, y + 1) map then
-                if Set.contains (x + 1, y + 1) map then Some (x, y)
-                else findRestPosition (x + 1, y + 1) map
-            else findRestPosition (x - 1, y + 1) map
-        else findRestPosition (x, y + 1) map
-    else None
+type SandUnitAction = MoveTo of int * int | Drop | Rest
 
-let rec fillWithSand sandUnits map =
-    match findRestPosition (500, 0) map with
+let rec findRestPosition coords getAction map =
+    match getAction coords map with
+    | Drop -> None
+    | MoveTo (x, y) -> findRestPosition (x, y) getAction map
+    | Rest -> Some coords
+
+let rec fillWithSand sandUnits getAction map =
+    match findRestPosition (500, 0) getAction map with
     | Some c ->
         let map' = Set.add c map
-        fillWithSand (sandUnits + 1)  map'
+        fillWithSand (sandUnits + 1) getAction map'
     | None -> sandUnits
 
-File.ReadLines("input.txt")
-|> Seq.fold parseRockLines Set.empty
-|> fillWithSand 0
-|> printfn "%A"
+let map =
+    File.ReadLines("input.txt")
+    |> Seq.fold parseRockLines Set.empty
+
+let maxY = map |> Set.toSeq |> Seq.map snd |> Seq.max
+let getAction (x, y) map =
+    if y = maxY then Drop
+    elif not <| Set.contains (x, y + 1) map then MoveTo (x, y + 1)
+    elif not <| Set.contains (x - 1, y + 1) map then MoveTo (x - 1, y + 1)
+    elif not <| Set.contains (x + 1, y + 1) map then MoveTo (x + 1, y + 1)
+    else Rest
+
+map
+|> fillWithSand 0 getAction
+|> printfn "Part 1: %d"
+
+let floor = maxY + 2
+let getAction2 (x, y) map =
+    if Set.contains (500, 0) map then Drop
+    elif y = floor - 1 then Rest
+    elif not <| Set.contains (x, y + 1) map then MoveTo (x, y + 1)
+    elif not <| Set.contains (x - 1, y + 1) map then MoveTo (x - 1, y + 1)
+    elif not <| Set.contains (x + 1, y + 1) map then MoveTo (x + 1, y + 1)
+    else Rest
+
+map
+|> fillWithSand 0 getAction2
+|> printfn "Part 1: %d"
