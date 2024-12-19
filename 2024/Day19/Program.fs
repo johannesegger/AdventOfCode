7b3739de8e8723fe109c46b1b0b5e4f23f8ac527
Johannes Egger <j.egger@posteo.at>
2024-12-19 17:28:31 +0100

    2024/19

---
+++ 2024/Day19/Program.fs
@@ -0,0 +1,45 @@
+﻿open Checked
+open System
+open System.IO
+
+let parse (lines: string[]) =
+    let towels = lines.[0].Split(',', StringSplitOptions.TrimEntries)
+    let patterns = lines.[2..]
+    (towels, patterns)
+
+let getCombinationCount (towels: string array) pattern =
+    let rec fn patterns =
+        match patterns with
+        | [] -> 0L
+        | [ ("", v) ] -> v
+        | patterns ->
+            let patterns' =
+                [
+                    for (pattern, count) in patterns do
+                        if pattern = "" then (pattern, count)
+                        for towel in towels do
+                            if pattern.StartsWith towel then
+                                (pattern.[towel.Length ..], count)
+                ]
+                |> List.groupBy fst
+                |> List.map (fun (pattern, counts) -> (pattern, counts |> Seq.sumBy snd))
+            fn patterns'
+
+    fn [(pattern, 1L)]
+
+let getPossiblePatterns (towels, patterns) =
+    patterns
+    |> Seq.map (getCombinationCount towels)
+
+File.ReadAllLines "input.txt"
+|> parse
+|> getPossiblePatterns
+|> Seq.filter (fun v -> v > 0)
+|> Seq.length
+|> printfn "Part 1: %d"
+
+File.ReadAllLines "input.txt"
+|> parse
+|> getPossiblePatterns
+|> Seq.sum
+|> printfn "Part 2: %d"
